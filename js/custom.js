146f35b206e4e68826f06794183315670ed357c3
Sumit Kumawat <kumawatsumit199@gmail.com>
2023-04-19 22:29:26 +0530

    js add

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(".slider").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        dots: true,
+        arrows: false,
+        // prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+        // nextArrow: '<button class="slide-arrow next-arrow"></button>',
+      });
+});
