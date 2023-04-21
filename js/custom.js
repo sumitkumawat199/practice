561551e1c714448d8b27dfcec0e07061a950de75
Sumit Kumawat <kumawatsumit199@gmail.com>
2023-04-21 22:13:40 +0530

    mobile-menu

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,21 @@
 $(document).ready(function () {
-    $(".slider").slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        dots: true,
-        arrows: false,
-        // prevArrow: '<button class="slide-arrow prev-arrow"></button>',
-        // nextArrow: '<button class="slide-arrow next-arrow"></button>',
-      });
+  $(".slider").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    dots: true,
+    arrows: false,
+    // prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+    // nextArrow: '<button class="slide-arrow next-arrow"></button>',
+  });
+});
+$(document).ready(function () {
+  $(".menu-bar").click(function () {
+    $(".mobile-menu").show("slow");
+  });
+  $(".close").click(function () {
+    $(".mobile-menu").hide("slow");
+  });
 });
